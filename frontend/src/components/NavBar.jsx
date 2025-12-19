4b2fc1e181b7486d5dc2e78793b85e18af0ccb59
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-18 22:56:06 -0500

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ export default function NavBar() {
   return (
     <div className="Navbar">
       <h1>NetWave</h1>
+      <p>work in progress</p>
       <img src={image} alt="" />
     </div>
   );
