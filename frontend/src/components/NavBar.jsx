ee427971d70087d1eed3a6d5526228c8f2e265f8
jefferyhoekstra <jefferyhoekstra@hotmail.com>
2025-12-18 23:53:37 -0500

    wavenetupdate

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import image from "../assets/images/8.gif";
 export default function NavBar() {
   return (
     <div className="Navbar">
-      <h1>NetWave</h1>
+      <h1>NetWave Blog</h1>
       <p>Coming soon!</p>
       <img src={image} alt="" />
     </div>
